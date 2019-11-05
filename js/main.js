b67d0904e1fcc4f83ab1f13d8523c287394db871
sarraHa <sara.benloulou@gmail.com>
2019-11-05 23:33:14 +0100

    #

--- js/main.js
+++ js/main.js
@@ -3,17 +3,26 @@ $(document).ready(function(){
 
 
     $nav = $('.nav');
-
-
     $toggleCollapse = $('.toggle-collapse');
 
-
-
+    
     /** click event on toggle menu **/ 
     $toggleCollapse.click(function(){
         $nav.toggleClass('collapse');
     })
  
+    // owel-carousel for blog
+    $('.owl-carousel').owlCarousel({
+        loop:true,
+        autoplay:true,
+        autoplayTimeout: 3000,
+        dots: false,
+        nav:true,
+        navText:[$('.owl-navigation .owl-nav-prev'), $('.owl-navigation .owl-nav-next')],
+        
+    });
 });  
+
+
     
 
